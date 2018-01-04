4f62a869e25d6d514f30a9a55fbc1ede740c43fb
Raymon S <raymon@raymonschouwenaar.nl>
2018-01-04 21:41:48 +0100

    Add socialcards

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,13 @@ console.log("🦊 Hello! Edit me in src/js/app.js");
 (() => {
     require('prismjs');
 
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js').then(registration => {
-                console.log('SW registered: ', registration);
-            }).catch(registrationError => {
-                console.log('SW registration failed: ', registrationError);
-            });
-        });
-    }
+    // if ('serviceWorker' in navigator) {
+    //     window.addEventListener('load', () => {
+    //         navigator.serviceWorker.register('/sw.js').then(registration => {
+    //             console.log('SW registered: ', registration);
+    //         }).catch(registrationError => {
+    //             console.log('SW registration failed: ', registrationError);
+    //         });
+    //     });
+    // }
 })();
