036186a938c3c004239b9b3b92f741becebc8707
Raymon S <raymon@raymonschouwenaar.nl>
2018-01-06 14:39:51 +0100

    Add justlazy

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,12 @@
 // JS Goes here - ES6 supported
+import justlazy from 'justlazy/src/justlazy.js';
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/js/app.js");
 (() => {
     require('prismjs');
+
+    justlazy.registerLazyLoadByClass("justlazy");
 
     // if ('serviceWorker' in navigator) {
     //     window.addEventListener('load', () => {
