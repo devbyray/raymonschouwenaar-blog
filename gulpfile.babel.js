6d15e15b4919a983d14a716eb8bcda8925cc9c82
Raymon S <raymon@raymonschouwenaar.nl>
2018-01-11 16:29:11 +0100

    Added cssnano and cloudinary gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,11 +9,37 @@ import BrowserSync from "browser-sync";
 import watch from "gulp-watch";
 import webpack from "webpack";
 import webpackConfig from "./webpack.conf";
-import workbox from 'workbox-build';
-import path from 'path';
+import workbox from "workbox-build";
+import path from "path";
+import cloudinary from "cloudinary";
+import es from "event-stream";
+import filenames from "gulp-filenames";
+import gulpCloudinary from "./custom-gulp-cloudinary";
+import cssnano from "gulp-cssnano";
+
+const config = {
+  cloud_name: "raymons",
+  api_key: "919626525222298",
+  api_secret: "zabj_4PHNAwy6a9azLgEvsLY--M"
+};
+
+var defaultTags = ["raymons-blog"];
+
+var builderDefault = new gulpCloudinary(config, defaultTags);
+
+gulp.task("clean", function() {
+  builderDefault.deleteOldByTag();
+});
+
+gulp.task("upload_images", function() {
+  return (gulp
+      // .src(["./site/static/wp-content/ray.jpg"])
+      .src(["./site/static/wp-content/**/*.{jpg,png,gif}"])
+      .pipe(builderDefault.uploader())
+      .pipe(gulp.dest("./dist/wp-content")));
+});
 
 const browserSync = BrowserSync.create();
-
 
 // Hugo arguments
 const hugoArgsDefault = ["-d", "../dist", "-s", "site", "-v"];
@@ -28,12 +54,14 @@ gulp.task("build", ["css", "js"], (cb) => buildSite(cb, [], "production"));
 gulp.task("build-preview", ["css", "js"], (cb) => buildSite(cb, hugoArgsPreview, "production"));
 
 // Compile CSS with PostCSS
-gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
+gulp.task("css", () =>
+  gulp
+    .src("./src/css/*.css")
     .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext()]))
+    .pipe(cssnano())
     .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
-));
+);
 // htaccess
 // gulp.task("other", () => (
 //   gulp.src([
@@ -44,14 +72,22 @@ gulp.task("css", () => (
 //     .pipe(browserSync.stream())
 // ));
 // Compile CSS with PostCSS
-gulp.task("images", () => (
-  gulp.src([
-    "./src/wp-content/**/*",
-    "./src/images/**/*"
-  ])
+var uploadToCloudinary = function(files) {
+  console.log("files: ", files);
+  // return files.map(function(file, cb) {
+  //   console.log(file.path);
+  //   return cb();
+  // });
+};
+
+gulp.task("images", () =>
+  gulp
+    .src(["./src/wp-content/**/*", "./src/images/**/*"])
+    .pipe(filenames("images"))
     .pipe(gulp.dest("./dist/wp-content"))
-    .pipe(browserSync.reload())
-));
+    // .pipe(browserSync.reload())
+    .pipe(console.log("images: ", filenames.get("images")))
+);
 
 // Compile Javascript
 gulp.task("js", (cb) => {
@@ -59,10 +95,13 @@ gulp.task("js", (cb) => {
 
   webpack(myConfig, (err, stats) => {
     if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
+    gutil.log(
+      "[webpack]",
+      stats.toString({
+        colors: true,
+        progress: true
+      })
+    );
     browserSync.reload();
     cb();
   });
@@ -76,9 +115,15 @@ gulp.task("server", ["hugo", "css", "js"], () => {
     },
     open: false
   });
-  watch("./src/js/**/*.js", () => { gulp.start(["js"]) });
-  watch("./src/css/**/*.css", () => { gulp.start(["css"]) });
-  watch("./site/**/*", () => { gulp.start(["hugo"]) });
+  watch("./src/js/**/*.js", () => {
+    gulp.start(["js"]);
+  });
+  watch("./src/css/**/*.css", () => {
+    gulp.start(["css"]);
+  });
+  watch("./site/**/*", () => {
+    gulp.start(["hugo"]);
+  });
 });
 
 /**
