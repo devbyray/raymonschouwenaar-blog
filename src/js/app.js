d057f989e00b3881015c396ebae632ea41cebe49
Raymon S <raymon@raymonschouwenaar.nl>
2018-01-14 12:18:48 +0100

    Added service worker

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,20 @@
 // JS Goes here - ES6 supported
 import justlazy from 'justlazy/src/justlazy.js';
+import 'prismjs';
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/js/app.js");
 (() => {
-    require('prismjs');
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/sw.js').then(registration => {
+                console.log('SW registered: ', registration);
+            }).catch(registrationError => {
+                console.log('SW registration failed: ', registrationError);
+            });
+        });
+    }
+
 
     justlazy.registerLazyLoadByClass("justlazy");
-
-    // if ('serviceWorker' in navigator) {
-    //     window.addEventListener('load', () => {
-    //         navigator.serviceWorker.register('/sw.js').then(registration => {
-    //             console.log('SW registered: ', registration);
-    //         }).catch(registrationError => {
-    //             console.log('SW registration failed: ', registrationError);
-    //         });
-    //     });
-    // }
 })();
