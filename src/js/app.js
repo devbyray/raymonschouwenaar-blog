47c06a68015d48209a741938e70845f495c83ce4
Raymon S <raymon@raymonschouwenaar.nl>
2018-01-19 16:29:11 +0100

    Created new visual styling

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,16 @@
 import justlazy from 'justlazy/src/justlazy.js';
 
 (() => {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js').then(registration => {
-                console.log('SW registered: ', registration);
-                registration.pushManager.subscribe({userVisibleOnly: true});
-            }).catch(registrationError => {
-                console.log('SW registration failed: ', registrationError);
-            });
-        });
-    }
+    // if ('serviceWorker' in navigator) {
+    //     window.addEventListener('load', () => {
+    //         navigator.serviceWorker.register('/sw.js').then(registration => {
+    //             console.log('SW registered: ', registration);
+    //             registration.pushManager.subscribe({userVisibleOnly: true});
+    //         }).catch(registrationError => {
+    //             console.log('SW registration failed: ', registrationError);
+    //         });
+    //     });
+    // }
 
     justlazy.registerLazyLoadByClass("justlazy");
 })();
--- src/js/single-post.js
+++ src/js/single-post.js
@@ -1,5 +1,23 @@
 // JS Goes here - ES6 supported
 import 'prismjs';
-(() => {
-    justlazy.registerLazyLoadByClass("justlazy");
+
+(function () {
+    console.log('🚀');
+    const tocButtonElement = document.querySelector('[data-element="tocButton"]');
+    const tocWrapperElement = document.querySelector('[data-element="tocWrapper"]');
+    const tocNavTextElement = document.querySelector('[data-element="tocNavText"]');
+    console.log('tocButtonElement: ', tocButtonElement);
+    console.log('tocWrapperElement: ', tocWrapperElement);
+    console.log('tocNavTextElement: ', tocNavTextElement);
+    let tocToggle = false;
+
+    tocButtonElement.addEventListener('click', function (event) {
+        if(!tocToggle) {
+            tocNavTextElement.innerHTML = 'Hide table of contents';
+        } else {
+            tocNavTextElement.innerHTML = 'Show table of contents';
+        }
+        tocToggle = !tocToggle;
+        tocWrapperElement.classList.toggle('toc--show');
+    });
 })();
