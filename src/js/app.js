f37e74ee24693bb6e24cfd0087f416781c01a87c
Raymon S <raymon@raymonschouwenaar.nl>
2018-01-25 20:44:28 +0100

    Moved SW to the top

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,16 @@
 import 'intersection-observer';
 
 (() => {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/sw.js').then(registration => {
+                console.log('SW registered: ', registration);
+            }).catch(registrationError => {
+                console.log('SW registration failed: ', registrationError);
+            });
+        });
+    }
+
     window.onload = function() {
         console.log('onload');
 
@@ -67,15 +77,6 @@ import 'intersection-observer';
             });
         }
 
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js').then(registration => {
-                    console.log('SW registered: ', registration);
-                }).catch(registrationError => {
-                    console.log('SW registration failed: ', registrationError);
-                });
-            });
-        }
     }
 
 })();
