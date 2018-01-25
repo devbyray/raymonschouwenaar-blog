1206eea1d5619f96f43cdf9516d7fa4b510b58af
Raymon S <raymon@raymonschouwenaar.nl>
2018-01-25 08:41:51 +0100

    Remove show comments button when clicked

--- src/js/single-post.js
+++ src/js/single-post.js
@@ -1,10 +1,11 @@
-// JS Goes here - ES6 supported
 (function () {
     console.log('🚀');
     const tocButtonElement = document.querySelector('[data-element="tocButton"]');
     const tocWrapperElement = document.querySelector('[data-element="tocWrapper"]');
     const tocNavTextElement = document.querySelector('[data-element="tocNavText"]');
+    const disqusCommentsButtonElement = document.querySelector('[data-element="disqusCommentsButton"]');
     const disqusShowCommentsElement = document.querySelector('[data-element="disqusShowComments"]');
+    const disqusCommentsElement = document.querySelector('[data-element="disqusComments"]');
     console.log('tocButtonElement: ', tocButtonElement);
     console.log('tocWrapperElement: ', tocWrapperElement);
     console.log('tocNavTextElement: ', tocNavTextElement);
@@ -22,20 +23,21 @@
         });
     }
 
-    if(disqusShowCommentsElement) {
-        disqusShowCommentsElement.addEventListener('click', function() {
+    if(disqusCommentsButtonElement) {
+        disqusCommentsButtonElement.addEventListener('click', function() {
 
-            // (function() {
-                if (["localhost", "127.0.0.1"].indexOf(window.location.hostname) != -1) {
-                    document.getElementById('disqus_thread').innerHTML = 'Disqus comments not available by default when the website is previewed locally.';
-                    return;
-                }
-                var d = document, s = d.createElement('script'); s.async = true;
-                s.src = '//' + window.DisqusShortname + '.disqus.com/embed.js';
-                s.setAttribute('data-timestamp', +new Date());
-                (d.head || d.body).appendChild(s);
-            // })();
+            console.log('disqusCommentsElement', disqusCommentsElement);
+            console.log('disqusShowCommentsElement', disqusShowCommentsElement);
+            disqusCommentsElement.removeChild(disqusShowCommentsElement);
 
+            if (["localhost", "127.0.0.1"].indexOf(window.location.hostname) != -1) {
+                document.getElementById('disqus_thread').innerHTML = 'Disqus comments not available by default when the website is previewed locally.';
+                return;
+            }
+            var d = document, s = d.createElement('script'); s.async = true;
+            s.src = '//' + window.DisqusShortname + '.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
 
         });
     }
