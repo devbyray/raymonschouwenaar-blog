9907ce10554da05dc6c22091b4fd41ff3de79948
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2018-03-16 21:34:59 +0100

    Fix issue with Service Worker for Safari 11

--- src/js/app.js
+++ src/js/app.js
@@ -5,21 +5,23 @@ import 'intersection-observer';
     console.log('Welcome to RAYs{FRONTEND}BYTES! Hopefully you like the 🚀 loading! If you have any trouble with it? Please hit me on https://twitter.com/rsschouwenaar');
 
 //Add this below content to your HTML page, or add the js file to your page at the very top to register sercie worker
-    if (navigator.serviceWorker.controller) {
+    if('serviceWorker' in navigator) {
+        if (navigator.serviceWorker.controller) {
 
-        console.log('[PWA Builder] active service worker found, no need to register')
+            console.log('[PWA Builder] active service worker found, no need to register')
 
-    } else {
+        } else {
 
-        //Register the ServiceWorker
-        navigator.serviceWorker.register('sw-custom.js', {
-            scope: './'
-        }).then(function(reg) {
-            console.log('Service worker has been registered for scope:'+ reg.scope);
-        });
+            //Register the ServiceWorker
+            navigator.serviceWorker.register('sw-custom.js', {
+                scope: './'
+            }).then(function(reg) {
+                console.log('Service worker has been registered for scope:'+ reg.scope);
+            });
+        }
     }
+
     window.onload = function() {
-        console.log('onload');
 
         const intersectionObserverOptions = {
             root: document.querySelector('body'),
@@ -30,11 +32,9 @@ import 'intersection-observer';
             // console.log('entries: ', entries);
             entries.forEach(entry => {
                 if (entry.intersectionRatio > 0) {
-                  console.log('in the view: ', entry);
+                //   console.log('in the view: ', entry);
                   progressiveImageLoading(entry.target);
                   io.unobserve(entry.target);
-                } else {
-                  console.log('out of view: ', entry);
                 }
             });
         });
@@ -47,14 +47,12 @@ import 'intersection-observer';
         function progressiveImageLoading(progressiveImageInput) {
             let imagePlaceholder = progressiveImageInput;
             let placholderSpacer = progressiveImageInput.querySelector('.spacer');
-            // console.log('progressiveImageInput: ', progressiveImageInput.querySelector('.placeholder'));
+
             // 1: load small image and show it
             if(!imagePlaceholder.classList.contains('image--loaded')) {
                 loadImage(imagePlaceholder, 'small').then(smallImage => {
                     imagePlaceholder.appendChild(smallImage);
                     loadImage(imagePlaceholder, 'large').then(placeholderImage => {
-                        // console.log('placeholderImage: ', placeholderImage)
-                        // console.log('imagePlaceholder: ', imagePlaceholder)
                         imagePlaceholder.appendChild(placeholderImage);
                         // placeholderImage.classList.add('large-loaded');
                     }).then(() => {
