136b8c66a7e4f192ac228ba7f4b609788d3ab927
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2018-03-25 22:37:43 +0200

    Fixed eslint for single-post.js

--- src/js/single-post.js
+++ src/js/single-post.js
@@ -1,15 +1,14 @@
-(function () {
-    const tocButtonElement = document.querySelector('[data-element="tocButton"]');
-    const tocWrapperElement = document.querySelector('[data-element="tocWrapper"]');
-    const tocNavTextElement = document.querySelector('[data-element="tocNavText"]');
-    let tocToggle = false;
+(() => {
+	const tocButtonElement = document.querySelector('[data-element="tocButton"]');
+	const tocWrapperElement = document.querySelector('[data-element="tocWrapper"]');
+	const tocNavTextElement = document.querySelector('[data-element="tocNavText"]');
+	let tocToggle = false;
 
-    if(tocButtonElement) {
-        tocButtonElement.addEventListener('click',  (event) => {
+	if (tocButtonElement) {
+		tocButtonElement.addEventListener('click', () => {
 			tocNavTextElement.innerHTML = !tocToggle ? 'Hide table of contents' : 'Show table of contents';
-            tocToggle = !tocToggle;
-            tocWrapperElement.classList.toggle('toc--show');
-        });
-    }
-
+			tocToggle = !tocToggle;
+			tocWrapperElement.classList.toggle('toc--show');
+		});
+	}
 })();
