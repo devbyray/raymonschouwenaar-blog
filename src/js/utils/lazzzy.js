7ca42a617243a9f760af3d572c7d4b601e7a71c3
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2018-03-25 22:47:46 +0200

    Fixed eslint issue for lazzzy.js

--- src/js/utils/lazzzy.js
+++ src/js/utils/lazzzy.js
@@ -1,52 +1,47 @@
 export default class Lazzzy {
-  constructor(progressiveImageInput) {
-	  this.imageInput = progressiveImageInput;
-  }
+	constructor(progressiveImageInput) {
+		this.imageInput = progressiveImageInput;
+	}
 
-  progressiveImageLoading() {
-    let imagePlaceholder = this.imageInput;
-    const placholderSpacer = this.imageInput.querySelector(".spacer");
+	progressiveImageLoading() {
+		const imagePlaceholder = this.imageInput;
+		const placholderSpacer = this.imageInput.querySelector('.spacer');
 
-    // 1: load small image and show it
-    if (!imagePlaceholder.classList.contains("image--loaded")) {
-      this.loadImage(imagePlaceholder, "small").then(smallImage => {
+		// 1: load small image and show it
+		if (!imagePlaceholder.classList.contains('image--loaded')) {
+			this.loadImage(imagePlaceholder, 'small').then((smallImage) => {
+				imagePlaceholder.appendChild(smallImage);
 
-		imagePlaceholder.appendChild(smallImage);
+				this.loadImage(imagePlaceholder, 'large').then((placeholderImage) => {
+					imagePlaceholder.appendChild(placeholderImage);
+				}).then(() => {
+					imagePlaceholder.removeChild(placholderSpacer);
+					imagePlaceholder.removeChild(imagePlaceholder.querySelector('.small-loaded'));
+					imagePlaceholder.classList.add('image--loaded');
+				});
+			});
+		}
+	}
 
-		this.loadImage(imagePlaceholder, "large")
-          .then(placeholderImage => {
-            imagePlaceholder.appendChild(placeholderImage);
-          })
-          .then(() => {
-			imagePlaceholder.removeChild(placholderSpacer);
-            imagePlaceholder.removeChild(imagePlaceholder.querySelector(".small-loaded"));
-            imagePlaceholder.classList.add("image--loaded");
-          });
-      });
-    }
-  }
+	static loadImage(imageSelector, typeImage) {
+		// Object destructering
+		// https://developer.mozilla.org/nl/docs/Web/JavaScript/Reference/Operatoren/Destructuring_assignment
+		const { dataset: { small: smallImg, large: largeImg } } = imageSelector;
 
-  loadImage(imageSelector, typeImage) {
-    // Object destructering
-    // https://developer.mozilla.org/nl/docs/Web/JavaScript/Reference/Operatoren/Destructuring_assignment
-    let { dataset: { small: smallImg, large: largeImg } } = imageSelector;
+		return new Promise((resolve, reject) => {
+			const img = new Image();
 
-    return new Promise((resolve, reject) => {
-      let img = new Image();
+			img.src = typeImage === 'small' ? smallImg : largeImg;
 
-      img.src = typeImage === "small" ? smallImg : largeImg;
+			img.onload = () => {
+				img.classList.add(typeImage === 'small' ? 'small-loaded' : 'large-loaded');
+				resolve(img);
+			};
 
-      img.onload = () => {
-        img.classList.add(
-          typeImage === "small" ? "small-loaded" : "large-loaded"
-        );
-        resolve(img);
-      };
-
-      img.onerror = () => {
-        console.error("Image could not be loaded: ", img);
-        reject(img);
-      };
-    });
-  }
+			img.onerror = () => {
+				console.error('Image could not be loaded: ', img);
+				reject(img);
+			};
+		});
+	}
 }
